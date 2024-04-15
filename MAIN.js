08a2a2b27ab8aeb6bae2c09fd7665b9a1a605557
naomierica11 <naomierica17@gmail.com>
2024-04-15 22:35:21 +0700

    update

--- MAIN.js
+++ MAIN.js
@@ -5,26 +5,42 @@ mntoggle.addEventListener('click',function(){
     nav.classList.toggle('menushow');    
 })
 
-// Jika yang dipilih adalah bagian untuk menampilkan hasil scraping
-if (contentId === 'scraped-data') {
-    // Lakukan request ke file JSON yang berisi hasil scraping
-    $.getJSON('republika_indeks.json', function(data){
-        var newsList = $('#news-list');
-        newsList.empty(); // Kosongkan daftar sebelum menambahkan data baru
+// Fungsi untuk menampilkan konten terkait saat menu dipilih
+document.querySelectorAll('nav a').forEach(function (menuItem) {
+    menuItem.addEventListener('click', function () {
+        var targetSectionId = this.getAttribute('href').substr(1);
+        showContent(targetSectionId);
+    });
+});
 
-        // Tambahkan setiap berita ke daftar
-        $.each(data, function(index, article){
-            if (article.hasOwnProperty('category')) {
-                var listItem = $('<li></li>');
+function showContent(contentId) {
+    document.querySelectorAll('section').forEach(function (section) {
+        section.style.display = 'none';
+    });
+
+    document.getElementById(contentId).style.display = 'block';
+
+    // Jika yang dipilih adalah bagian untuk menampilkan hasil scraping
+    if (contentId === 'scraped-data') {
+        // Lakukan request ke file JSON yang berisi hasil scraping
+        $.getJSON('republika_indeks.json', function(data){
+            var newsList = $('#news-list');
+            newsList.empty(); // Kosongkan daftar sebelum menambahkan data baru
+
+            // Tambahkan setiap berita ke daftar
+            data.forEach(function(article, index) {
+                var listItem = $('<tr></tr>');
                 var datetime = new Date(article.scrape_time);
                 listItem.html(
-                    '<strong>' + article.title + '</strong><br>' +
-                    '<em>Kategori: ' + article.category + '</em><br>' +
-                    'Waktu Publish: ' + article.publish_time + '<br>' +
-                    'Waktu Scraping: ' + datetime.toLocaleString()
+                    '<td>' + (index + 1) + '</td>' +
+                    '<td>' + article.title + '</td>' +
+                    '<td>' + article.category + '</td>' +
+                    '<td>' + article.publish_time + '</td>' +
+                    '<td>' + datetime.toLocaleString() + '</td>'
                 );
                 newsList.append(listItem);
-            }
+            });
         });
-    });
+    }
 }
+
